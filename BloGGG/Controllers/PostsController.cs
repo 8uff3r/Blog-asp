edda316c718bf3a6d9ff2b75ba3fb4b414bb5c74
8uff3r <8uff3r@gmail.com>
2023-05-11 16:34:15 +0330

    Last Before Htmx

--- BloGGG/Controllers/PostsController.cs
+++ BloGGG/Controllers/PostsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
 
 namespace BloGGG.Controllers;
 
@@ -66,17 +67,20 @@ public class PostsController : Controller
         var result = await (from p in _context.Posts
             where p.ID == id
             select p).FirstOrDefaultAsync();
+        Console.WriteLine(result.ToJson());
         return View(result);
     }
 
     public async Task<IActionResult> Update(PostModel postModel)
     {
+        postModel.PostTags = postModel.PostTagsString.Replace(" ", "").Split(",");
         var result = await (from p in _context.Posts
             where p.ID == postModel.ID
             select p).FirstOrDefaultAsync();
         if (result != null)
         {
             _context.Update(result);
+            result.PostTags = postModel.PostTags;
             result.PostTitle = postModel.PostTitle;
             result.PostBody = postModel.PostBody;
             await _context.SaveChangesAsync();
